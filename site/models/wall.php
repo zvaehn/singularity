e4d7c5743b75fe106b0097e9aaa341ef08aad943
Sven Schiffer <svenschiffer92@gmail.com>
2016-08-27 19:07:25 +0200

    wall controller to model and proper caching

---
+++ site/models/wall.php
@@ -0,0 +1,47 @@
+<?php
+DEFINE('CACHEDIR', kirby()->roots()->cache(). DS . 'wall');
+DEFINE('CACHEFILE', 'wall.json');
+
+class WallPage extends Page {
+
+  private $cachefile = CACHEDIR . DS . CACHEFILE;
+
+  function cacheNeedsRebuild() {
+    // CACHE REBUILD
+    $now = time();
+    $cacheDuration = $this->cacheDuration()->int(); // * 60;
+    $expireAt = $this->touched()->int() + $cacheDuration;
+    $rebuildCache = ($now >= $expireAt) ? true : false;
+
+    /*echo "now: ". $now."<br>";
+    echo "cache duration: ".$cacheDuration."s<br>";
+    echo "expire at : ".$expireAt."<br>";
+    echo "touched at: ".$page->touched()->int()."<br>";
+    echo "rebuild: ".$rebuildCache;*/
+
+    return $rebuildCache || !$this->cacheAvailable();
+  }
+
+  function cacheAvailable() {
+    return is_readable($this->cachefile);
+  }
+
+  function getCache() {
+    if(!$this->cacheAvailable() || $this->cacheNeedsRebuild()) return false;
+
+    // GET CACHE DATA
+    @mkdir(CACHEDIR, true);
+    @$rawCache = file_get_contents(CACHEDIR . DS . CACHEFILE);
+
+    if(!$rawCache) {
+      return false;
+    }
+    else {
+      return json_decode($rawCache, true);
+    }
+  }
+
+  function setCache($data) {
+    return file_put_contents($this->cachefile, json_encode($data));
+  }
+}
--- site/templates/wall.php
+++ site/templates/wall.php
@@ -3,26 +3,41 @@
   <main class="main" role="main">
 
   <?php
-    if($rebuildCache) {
+    $data = $page->getCache();
+
+    // if cache is not available or outdated, rebuild it
+    if(!$data) {
+      unset($data);
+      $data = array();
+
       echo "rebuilding cache...<br>";
 
       $page->update(array('touched' => time()));
 
-      $instagram = c::get('integrations')['instagram'];
       $instagramPage = page('integrations/instagram');
 
-      if($instagramPage->accessToken() && $instagramPage->isVisibleOnWall()->bool()) {
-        $instagram->setAccessToken($instagramPage->accessToken()->value());
+      if($instagramPage->isVisibleOnWall()->value()) {
+        $instagram = c::get('integrations')['instagram'];
 
-        $media = $instagram->getUserMedia('self', 10);
+        // Check if instagram is enabled and valid
+        if($instagramPage->accessToken()) {
+          $instagram->setAccessToken($instagramPage->accessToken()->value());
+          $media = $instagram->getUserMedia('self', 10);
 
-        foreach ($media->data as $entry) {
-          echo "<img src=\"{$entry->images->thumbnail->url}\">";
+          // Update the cache
+          $data['instagram'] = json_decode(json_encode($media->data), true);
         }
       }
+
+      /*echo "<pre>";
+      print_r($data);
+      echo "</pre>";*/
+
+      $page->setCache($data);
     }
-    else {
-      echo "cache up to date";
+
+    foreach ($data['instagram'] as $entry) {
+      echo "<img src=\"{$entry['images']['thumbnail']['url']}\">";
     }
   ?>
 

a0ebce0f63697d12f5481a461ca3015950c4775d
Sven Schiffer <svenschiffer92@gmail.com>
2017-03-02 16:36:21 +0100

    improved ga support

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -35,21 +35,6 @@
     echo file_get_contents('assets/compiled/style.min.css');
     echo "</style>";
   }
-
-  if(!c::get('development') && $site->analytics()->value()) {
-    ?>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','<?= url("assets/compiled/script.min.js") ?>','ga');
-
-      ga('create', '<?= $site->analytics()->value() ?>', 'auto');
-      ga('send', 'pageview');
-
-    </script>
-    <?php
-  }
   ?>
 </head>
 <body>

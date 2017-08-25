21567a060175c4af18b825fee058672a434c279c
Sven Schiffer <svenschiffer92@gmail.com>
2017-08-25 15:49:53 +0200

    fixed font implementation oO. Readded domainsharding

--- site.php
+++ site.php
@@ -3,10 +3,14 @@ $kirby = kirby();
 
 require 'lib/phpflickr-master/phpFlickr.php';
 
+$domainShardCounter = 1;
+
 function get_compressorUrl($url) {
   $ext = $ext = pathinfo($url, PATHINFO_EXTENSION);
   $filename = sha1($url) . "." . $ext;
+
   $path = "/assets/imgcache/";
+  $domainShards = 3;
 
   // replacing rules
   $url = str_replace('/', '__-0-__', $url);
@@ -18,9 +22,17 @@ function get_compressorUrl($url) {
     $fullUrl = "//" . $_SERVER['HTTP_HOST'] . $path . '__--__' . $encodedUrl;
   }
   else {
-    $fullUrl = "//" . "static.hiimzvaehn.de" . $path . '__--__' . $encodedUrl;
-  }
+    // $fullUrl = "//" . "static.hiimzvaehn.de" . $path . '__--__' . $encodedUrl;
 
+    global $domainShardCounter;
+    $domainShardCounter++;
+
+    if($domainShardCounter > $domainShards) {
+      $domainShardCounter = 1;
+    }
+
+    $fullUrl = "//" . "static" . $domainShardCounter . ".hiimzvaehn.de" . $path . '__--__' . $encodedUrl;
+  }
 
   return $fullUrl;
 }

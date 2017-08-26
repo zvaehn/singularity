07c420a967c0416d8a0bcc9a89afd4879a0075f6
Sven Schiffer <svenschiffer92@gmail.com>
2017-08-26 13:47:54 +0200

    fixed plugin bug

--- site/plugins/minifyhtml/minifyhtml.php
+++ site/plugins/minifyhtml/minifyhtml.php
@@ -1,11 +1,13 @@
 <?php
 
-if (!c::get('MinifyHTML')) {
-	require_once(__DIR__ . DS . 'library' . DS . 'Minify_HTML.php');
+if (c::get('MinifyHTML')) {
+	// require_once(__DIR__ . DS . 'library' . DS . 'Minify_HTML.php');
 
-	function minifyhtml($buffer)
-	{
-	    return Minify_HTML::minify($buffer);
+	function minifyhtml($buffer) {
+		return preg_replace(
+			 '/(?>[^\S ]\s*|\s{2,})(?=(?:(?:[^<]++|<(?!\/?(?:textarea|pre)\b))*+)
+			 (?:<(?>textarea|pre)\b|\z))/ix', '', $buffer
+	 );
 	}
 
 	ob_start('minifyhtml');
